52b38221fc1aa1f624b168aef747b229a79a1dca
sw02020 <cowper02@naver.com>
2021-12-04 13:38:01 +0900

    sw02020

--- src/DeckButton.java
+++ src/DeckButton.java
@@ -1,9 +1,15 @@
+import javax.swing.JButton;
+import java.awt.event.*;
 
-public class DeckButton {
+public class DeckButton extends JButton implements ActionListener{
+	
+	private OneCard game;
+	private MainGameFrame gameFrame;
 	public DeckButton(OneCard g, MainGameFrame gf) {
-		
+		game = g;
+		gameFrame = gf;
 	}
 	public void actionPerformed(ActionEvent e) {
-		// gameFrame.deckClicked();s
+		gameFrame.deckClicked();
 	}
 }
